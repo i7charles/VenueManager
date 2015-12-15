659c007f5d588d61af454341573903b23782f064
Charles Billette <charles.billette@tc.tc>
2015-12-15 11:20:34 -0500

    Initial Commit

---
+++ VenueManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VenueManager
+//
+//  Created by Charles Billette on 2015-12-15.
+//  Copyright © 2015 Charles Billette. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
